944a31574bf9a37e7556f905184fac110ef0e750
Marian Marian <fenrir55@gmail.com>
2021-07-14 21:06:29 +0200

    DeleteIuserRepository

---
+++ ConfArch.Web/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace ConfArch.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime CareerStarted { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
